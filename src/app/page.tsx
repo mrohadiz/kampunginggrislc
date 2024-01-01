d197d3273dea82d8c06879b68f17d21e222ebdef
AlgebraIbrahim <algebraibrahim@gmail.com>
2024-01-01 22:55:25 +0700

    add google tag manager

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,23 @@ import Company from '@/components/Company'
 import Program from '@/components/Program'
 import Keseruan from '@/components/Keseruan'
 import Timeline from '@/components/Timeline'
-import Head from 'next/head'
+import Script from 'next/script'
 
 export default function Home() {
   return (
     <main className="ease-in duration-300">
+
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+      </Script>
+
       <Hero />
       <CTA />
       <Timeline />
